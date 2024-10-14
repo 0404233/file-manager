deac38dd85e51b8a84091f4f085903cc12ff1f59
0404233 <andrey0404233@gmail.com>
2024-10-15 01:49:25 +0300

    feat: navigation and working directory operations implemented

--- index.mjs
+++ index.mjs
@@ -1,7 +1,8 @@
 import { homedir } from 'os';
-import { dirname } from 'path';
+import { dirname, join, parse } from 'path';
 import { fileURLToPath } from 'url';
 import { chdir, cwd } from 'process';
+import { readdir, stat } from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -24,6 +25,7 @@ console.log(`Welcome to the File Manager, ${username}!`);
 
 let currentLocation = homedir();
 chdir(currentLocation);
+console.log(`You are currently in ${cwd()}`);
 
 process.on('SIGINT', () => {
   console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
@@ -32,15 +34,59 @@ process.on('SIGINT', () => {
 });
 
 const doCommand = async (input) => {
-  if (input === '.exit') {
-    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-    console.log(`You are currently in ${cwd()}`);
-    process.exit();
+
+  const [operation, ...files] = input.split(' ');
+
+  switch (operation) {
+    case 'up':
+      const upFolder = join(currentLocation, '..');
+      chdir(upFolder);
+      currentLocation = cwd();
+      console.log(`You are currently in ${cwd()}`);
+    break;
+
+    case 'cd':
+      const chooseFolder = files[0];
+      try {
+        chdir(chooseFolder);
+        currentLocation = cwd();
+        console.log(`You are currently in ${cwd()}`);
+      } catch {
+        console.log('Invalid input');
+      }
+    
+    break;
+
+    case 'ls':
+      try {
+        const files = await readdir(currentLocation);
+        const formatOfFile = await Promise.all(files.map(async file => {
+          const format = await stat(join(currentLocation, file));
+
+          return {
+            name: file,
+            type: format.isDirectory() ? 'directory' : 'file'
+          };
+        }));
+        
+        formatOfFile.sort((a, b) => a.type.localeCompare(b.type) || a.name.localeCompare(b.name));
+        formatOfFile.forEach(file => {
+          console.log(`${file.name} : ${file.type}`);
+        })
+      } catch {
+        console.log('Invalid input');
+      }
+    
+      break;
+
+    case '.exit':
+      console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+      console.log(`You are currently in ${cwd()}`);
+      process.exit();
   }
 }
 
 const createInputLine = () => {
-  console.log(`You are currently in ${cwd()}`);
 
   process.stdin.on('data', (input) => {
     doCommand(input.toString().trim());
